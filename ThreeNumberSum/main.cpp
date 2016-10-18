b8c1ff32a33e0dc99bf38b3905f4a6558d7bceee
陈宇峰 <yufengcode@gmail.com>
2016-10-18 22:09:12 +0800

    1

--- ThreeNumberSum/main.cpp
+++ ThreeNumberSum/main.cpp
@@ -7,9 +7,76 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  三数和问题
+ *
+ */
+
+void ThreeNumberSum(vector<int> vec, int sum);
+
+int main(int argc, const char * argv[])
+{
+    int arr[] = {5,1,9,0,4,2,7,8};
+    vector<int> vv(arr, arr + sizeof(arr)/sizeof(int));
+    ThreeNumberSum(vv, 13);
+
+
     return 0;
 }
+
+void ThreeNumberSum(vector<int> vec, int sum)
+{
+    long p,q,r;
+    // 先排序
+    sort(vec.begin(), vec.end());
+
+    for (p = 0; p < vec.size() - 2; p++)
+    {
+        q = p + 1;
+        r = vec.size() - 1;
+        int remain = sum  - vec[p];
+        while (q < r)
+        {
+            int temp = vec[q] + vec[r];
+            if (temp == remain)
+            {
+                // 找到这三个数
+                cout << vec[p] << " " << vec[q] << " " << vec[r] << endl;
+                break;
+            }
+            else if(temp < remain)
+            {
+                q++;
+            }
+            else if (temp > remain)
+            {
+                r--;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
